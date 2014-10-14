02baca75480aa3224715d374f07666f3bc2ea8b1
Efim <egraev@acm.org>
2014-10-14 23:17:19 +0400

    Changed update time

--- src/ua/dp/isd/ypys/Globals.java
+++ src/ua/dp/isd/ypys/Globals.java
@@ -11,7 +11,7 @@ public class Globals
 	public static final String TEMP_PREFIX = "";
 	public static final String TEMP_SUFFIX = "";
 	public static final String TEMP_URL = "http://www.sevmeteo.info/data/temp.htm";
-	public static final int UPDATE_INTERVAL = 5 * 60 * 1000;
+	public static final int UPDATE_INTERVAL = 10 * 60 * 1000;
 	public static final String TEMP_SEARCH_TEXT = "Текущая температура воздуха";
 
 }
